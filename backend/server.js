c355fff0e423e50b4e32e0fd3873e2532447e06b
fowosere ademola <fowosereademola@gmail.com>
2024-08-12 19:31:35 +0100

    installed cors

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -9,18 +10,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader("Access-Control-Allow-Headers", "Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Support Desk API" });
